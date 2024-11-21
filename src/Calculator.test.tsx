11cec50ecfe6a074a6aba91e9cda1895c40ce250
Tej Powar <tej@powtech.co.uk>
2024-11-21 13:42:39 +0000

    add tailwind. Write empty tests for the creation of the calculator

---
+++ src/Calculator.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+describe('Calculator', () => {
+  test('render container layout of a calculator with title calculator', () => {
+  });
+
+  test('render entry screen for calculator', () => {
+  });
+
+  test('should display buttons with numbers', () => {
+
+  });
+
+  test('have five operands shown on the screen', () => {
+
+  });
+
+  test('should display the number clicked on the screen', () => {
+
+  });
+
+  test('should add two numbers correctly', () => {});
+
+  test('should minus two numbers correctly', () => {});
+
+  test('should divide two numbers correctly', () => {});
+
+  test('should multiply two numbers correctly', () => {});
+
+});
---
+++ src/Calculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Calculator() {
+  return (
+      <div className="App">
+
+      </div>
+  );
+}
+
+export default Calculator;
